c7a0400248e3a2d9e21e9eaf36002264202657a6
Vinicius Ferreira <vcf@dsi.uminho.pt>
2026-01-22 13:47:08 +0000

    Dockerfile of CORE 9.0.3 + running script
    Dockerfile.ubuntu - Ubuntu container with CORE 9.0.3 + IRC TP dependencies + ssh server
    run-docker.sh - Script to run CORE 9.0.3 container from vinicf Dockerhub repo. It creates a shared volume defined in SHARED variable ($SHARED:/shared), shares the host display to run core-gui. It also forwards the host's 2000 port to container's 22 and injects the ~/.ssh/id_ed25519.pub as an authorized host key (does not create it previously).

---
+++ run-docker.sh
@@ -0,0 +1,53 @@
+#!/bin/sh
+
+SHARED="$HOME/IRC/shared"
+PLATFORM=$(uname)
+
+if [ -n "$1" ]; then
+    SHARED=$(readlink -f $1)
+    echo Using custom shared directory: $SHARED
+else 
+    echo Using default shared directory: $SHARED
+fi
+
+if [ "$PLATFORM" = "Darwin" ]; then
+xhost + 127.0.0.1
+docker run -itd --rm \
+    --name core \
+    -p 2000:22 \
+    -p 50051:50051 \
+    -v $SHARED:/shared \
+    -e SSHKEY="`cat ~/.ssh/id_ed25519.pub`" \
+    -e DISPLAY=host.docker.internal:0 \
+    --privileged \
+    --entrypoint core-daemon \
+    vinicf/core-9.0.3:latest
+else
+xhost +local:root
+docker run -itd --rm \
+    --name core \
+    -p 2000:22 \
+    -p 50051:50051 \
+    -v $SHARED:/shared \
+    -e SSHKEY="`cat ~/.ssh/id_ed25519.pub`" \
+    -e DISPLAY \
+    -v /tmp/.X11-unix:/tmp/.X11-unix:rw \
+    --privileged \
+    --entrypoint core-daemon \
+    vinicf/core-9.0.3:latest
+fi
+# afterwards you may run
+sleep 3
+status=$(docker ps --filter status=running | grep core)
+while true; do
+    if [ -n "$status" ]; then
+        docker exec  core service ssh start
+        docker exec -it core core-gui
+        break
+    else 
+        sleep 1
+        status=$(docker ps --filter status=running | grep core)
+    fi
+done
+
+docker stop core
